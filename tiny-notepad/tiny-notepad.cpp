a6ee66ca2fb3860e6b91a95cd8acf69ee10827f3
wlbf <lbf1353@live.com>
2017-02-12 17:51:37 +0800

    add open save menu

--- tiny-notepad/tiny-notepad.cpp
+++ tiny-notepad/tiny-notepad.cpp
@@ -3,20 +3,10 @@
 
 #include "stdafx.h"
 #include "tiny-notepad.h"
+#include <commdlg.h>
+#include <commctrl.h>
 
-#define MAX_LOADSTRING 100
-#define IDC_MAIN_EDIT 101
-
-// Global Variables:
-HINSTANCE hInst;                                // current instance
-WCHAR szTitle[MAX_LOADSTRING];                  // The title bar text
-WCHAR szWindowClass[MAX_LOADSTRING];            // the main window class name
-
-// Forward declarations of functions included in this code module:
-ATOM                MyRegisterClass(HINSTANCE hInstance);
-BOOL                InitInstance(HINSTANCE, int);
-LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
-INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
+#pragma comment(lib, "comctl32.lib")
 
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
                      _In_opt_ HINSTANCE hPrevInstance,
@@ -39,18 +29,13 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
         return FALSE;
     }
 
-    HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_TINYNOTEPAD));
-
     MSG msg;
 
     // Main message loop:
     while (GetMessage(&msg, nullptr, 0, 0))
     {
-        if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
-        {
-            TranslateMessage(&msg);
-            DispatchMessage(&msg);
-        }
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
     }
 
     return (int) msg.wParam;
@@ -120,13 +105,68 @@ BOOL initEditInstance(HWND hWnd)
     hEdit = CreateWindowEx(WS_EX_CLIENTEDGE, _T("EDIT"), _T(""),
         WS_CHILD | WS_VISIBLE | WS_VSCROLL | WS_HSCROLL | ES_MULTILINE | ES_AUTOVSCROLL | ES_AUTOHSCROLL,
         0, 0, 100, 100, hWnd, (HMENU)IDC_MAIN_EDIT, GetModuleHandle(NULL), NULL);
-    if (hEdit == NULL) return FALSE;
-
+    if (hEdit == NULL)
+    {
+        MessageBox(hWnd, _T("Could not create main edit."), _T("Error"), MB_OK | MB_ICONERROR);
+        return FALSE;
+    }
     hfDefault = (HFONT)GetStockObject(DEFAULT_GUI_FONT);
     SendMessage(hEdit, WM_SETFONT, (WPARAM)hfDefault, MAKELPARAM(FALSE, 0));
     return TRUE;
 }
 
+void initToolBar(HWND hWnd)
+{
+    HWND hTool;
+    TBBUTTON tbb[3];
+    TBADDBITMAP tbab;
+
+    // Create Toolbar
+    hTool = CreateWindowEx(0, TOOLBARCLASSNAME, NULL, WS_CHILD | WS_VISIBLE, 0, 0, 0, 0,
+        hWnd, (HMENU)IDC_MAIN_TOOL, GetModuleHandle(NULL), NULL);
+    if (hTool == NULL)
+        MessageBox(hWnd, _T("Could not create tool bar."), _T("Error"), MB_OK | MB_ICONERROR);
+
+    // Send the TB_BUTTONSTRUCTSIZE message, which is required for
+    // backward compatibility.
+    SendMessage(hTool, TB_BUTTONSTRUCTSIZE, (WPARAM)sizeof(TBBUTTON), 0);
+
+    tbab.hInst = HINST_COMMCTRL;
+    tbab.nID = IDB_STD_SMALL_COLOR;
+    SendMessage(hTool, TB_ADDBITMAP, 0, (LPARAM)&tbab);
+
+    ZeroMemory(tbb, sizeof(tbb));
+    tbb[0].iBitmap = STD_FILENEW;
+    tbb[0].fsState = TBSTATE_ENABLED;
+    tbb[0].fsStyle = TBSTYLE_BUTTON;
+    tbb[0].idCommand = ID_FILE_NEW;
+
+    tbb[1].iBitmap = STD_FILEOPEN;
+    tbb[1].fsState = TBSTATE_ENABLED;
+    tbb[1].fsStyle = TBSTYLE_BUTTON;
+    tbb[1].idCommand = ID_FILE_OPEN;
+
+    tbb[2].iBitmap = STD_FILESAVE;
+    tbb[2].fsState = TBSTATE_ENABLED;
+    tbb[2].fsStyle = TBSTYLE_BUTTON;
+    tbb[2].idCommand = ID_FILE_SAVE;
+
+    SendMessage(hTool, TB_ADDBUTTONS, sizeof(tbb) / sizeof(TBBUTTON), (LPARAM)&tbb);
+}
+
+void initStatusBar(HWND hWnd)
+{
+    HWND hStatus;
+    int statwidths[] = { 100, -1 };
+
+    // Create Status bar
+    hStatus = CreateWindowEx(0, STATUSCLASSNAME, NULL,
+        WS_CHILD | WS_VISIBLE | SBARS_SIZEGRIP, 0, 0, 0, 0,
+        hWnd, (HMENU)IDC_MAIN_STATUS, GetModuleHandle(NULL), NULL);
+
+    SendMessage(hStatus, SB_SETPARTS, sizeof(statwidths) / sizeof(int), (LPARAM)statwidths);
+    SendMessage(hStatus, SB_SETTEXT, 0, (LPARAM)_T("Hi there :)"));
+}
 //
 //  FUNCTION: WndProc(HWND, UINT, WPARAM, LPARAM)
 //
@@ -142,17 +182,50 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
     switch (message)
     {
     case WM_CREATE:
-        initEditInstance(hWnd);
+        {
+            initEditInstance(hWnd);
+            initStatusBar(hWnd);
+            initToolBar(hWnd);
+        }
         break;
     case WM_SIZE:
         {
+            HWND hTool;
+            RECT rcTool;
+            int iToolHeight;
+
+            HWND hStatus;
+            RECT rcStatus;
+            int iStatusHeight;
+
             HWND hEdit;
+            int iEditHeight;
             RECT rcClient;
+
+            // Size toolbar and get height
+
+            hTool = GetDlgItem(hWnd, IDC_MAIN_TOOL);
+            SendMessage(hTool, TB_AUTOSIZE, 0, 0);
+
+            GetWindowRect(hTool, &rcTool);
+            iToolHeight = rcTool.bottom - rcTool.top;
+
+            // Size status bar and get height
+
+            hStatus = GetDlgItem(hWnd, IDC_MAIN_STATUS);
+            SendMessage(hStatus, WM_SIZE, 0, 0);
+
+            GetWindowRect(hStatus, &rcStatus);
+            iStatusHeight = rcStatus.bottom - rcStatus.top;
+
+            // Calculate remaining height and size edit
 
             GetClientRect(hWnd, &rcClient);
 
+            iEditHeight = rcClient.bottom - iToolHeight - iStatusHeight;
+
             hEdit = GetDlgItem(hWnd, IDC_MAIN_EDIT);
-            SetWindowPos(hEdit, NULL, 0, 0, rcClient.right, rcClient.bottom, SWP_NOZORDER);
+            SetWindowPos(hEdit, NULL, 0, iToolHeight, rcClient.right, iEditHeight, SWP_NOZORDER);
         }
         break;
     case WM_COMMAND:
@@ -161,10 +234,25 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
             // Parse the menu selections:
             switch (wmId)
             {
-            case IDM_ABOUT:
-                DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
+            case ID_FILE_NEW:
+                DoFileNew(hWnd);
                 break;
-            case IDM_EXIT:
+            case ID_FILE_OPEN:
+                DoFileOpen(hWnd);
+                break;
+            case ID_FILE_SAVE:
+                DoFileSave(hWnd);
+                break;
+            case ID_EDIT_CUT:
+                SendDlgItemMessage(hWnd, IDC_MAIN_EDIT, WM_CUT, 0, 0);
+                break;
+            case ID_EDIT_COPY:
+                SendDlgItemMessage(hWnd, IDC_MAIN_EDIT, WM_COPY, 0, 0);
+                break;
+            case ID_EDIT_PASTE:
+                SendDlgItemMessage(hWnd, IDC_MAIN_EDIT, WM_PASTE, 0, 0);
+                break;
+            case ID_FILE_EXIT:
                 DestroyWindow(hWnd);
                 break;
             default:
@@ -189,22 +277,136 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
     return 0;
 }
 
-// Message handler for about box.
-INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+void DoFileNew(HWND hWnd)
 {
-    UNREFERENCED_PARAMETER(lParam);
-    switch (message)
-    {
-    case WM_INITDIALOG:
-        return (INT_PTR)TRUE;
-
-    case WM_COMMAND:
-        if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+        HWND hEdit = GetDlgItem(hWnd, IDC_MAIN_EDIT);
+        if (SetWindowTextA(hEdit, (LPCSTR)""))
         {
-            EndDialog(hDlg, LOWORD(wParam));
-            return (INT_PTR)TRUE;
+            SendDlgItemMessage(hWnd, IDC_MAIN_STATUS, SB_SETTEXT, 0, (LPARAM)_T("Opened..."));
+            SendDlgItemMessage(hWnd, IDC_MAIN_STATUS, SB_SETTEXT, 1, (LPARAM)_T(""));
         }
-        break;
+}
+
+void DoFileOpen(HWND hWnd)
+{
+    OPENFILENAME ofn;
+    TCHAR szFileName[MAX_PATH] = _T("");
+
+    ZeroMemory(&ofn, sizeof(ofn));
+
+    ofn.lStructSize = sizeof(OPENFILENAME);
+    ofn.hwndOwner = hWnd;
+    ofn.lpstrFilter = _T("Text Files (*.txt)\0*.txt\0All Files (*.*)\0*.*\0");
+    ofn.lpstrFile = szFileName;
+    ofn.nMaxFile = MAX_PATH;
+    ofn.Flags = OFN_EXPLORER | OFN_FILEMUSTEXIST | OFN_HIDEREADONLY;
+    ofn.lpstrDefExt = _T("txt");
+
+    if (GetOpenFileName(&ofn))
+    {
+        HWND hEdit = GetDlgItem(hWnd, IDC_MAIN_EDIT);
+        if (LoadTextFileToEdit(hEdit, szFileName))
+        {
+            SendDlgItemMessage(hWnd, IDC_MAIN_STATUS, SB_SETTEXT, 0, (LPARAM)_T("Opened..."));
+            SendDlgItemMessage(hWnd, IDC_MAIN_STATUS, SB_SETTEXT, 1, (LPARAM)szFileName);
+        }
     }
-    return (INT_PTR)FALSE;
+}
+
+void DoFileSave(HWND hWnd)
+{
+    OPENFILENAME ofn;
+    TCHAR szFileName[MAX_PATH] = _T("");
+
+    ZeroMemory(&ofn, sizeof(ofn));
+
+    ofn.lStructSize = sizeof(OPENFILENAME);
+    ofn.hwndOwner = hWnd;
+    ofn.lpstrFilter = _T("Text Files (*.txt)\0*.txt\0All Files (*.*)\0*.*\0");
+    ofn.lpstrFile = szFileName;
+    ofn.nMaxFile = MAX_PATH;
+    ofn.lpstrDefExt = _T("txt");
+    ofn.Flags = OFN_EXPLORER | OFN_PATHMUSTEXIST | OFN_HIDEREADONLY | OFN_OVERWRITEPROMPT;
+
+    if (GetSaveFileName(&ofn))
+    {
+        HWND hEdit = GetDlgItem(hWnd, IDC_MAIN_EDIT);
+        if (SaveTextFileFromEdit(hEdit, szFileName))
+        {
+            SendDlgItemMessage(hWnd, IDC_MAIN_STATUS, SB_SETTEXT, 0, (LPARAM)_T("Saved..."));
+            SendDlgItemMessage(hWnd, IDC_MAIN_STATUS, SB_SETTEXT, 1, (LPARAM)szFileName);
+        }
+    }
+}
+
+BOOL LoadTextFileToEdit(HWND hEdit, LPCTSTR pszFileName)
+{
+    HANDLE hFile;
+    BOOL bSuccess = FALSE;
+
+    hFile = CreateFile(pszFileName, GENERIC_READ, FILE_SHARE_READ, NULL,
+        OPEN_EXISTING, 0, NULL);
+    if (hFile != INVALID_HANDLE_VALUE)
+    {
+        DWORD dwFileSize;
+
+        dwFileSize = GetFileSize(hFile, NULL);
+        if (dwFileSize != INVALID_FILE_SIZE)
+        {
+            LPVOID pszFileText;
+
+            HANDLE heapHandle = GetProcessHeap();
+            pszFileText = HeapAlloc(heapHandle, HEAP_GENERATE_EXCEPTIONS, dwFileSize + 1);
+            if (pszFileText != NULL)
+            {
+                DWORD dwRead;
+
+                if (ReadFile(hFile, pszFileText, dwFileSize, &dwRead, NULL))
+                {
+                    ((char*)pszFileText)[dwFileSize] = 0;// Add null terminator
+                    if (SetWindowTextA(hEdit, (LPCSTR)pszFileText))
+                        bSuccess = TRUE;
+                }
+                HeapFree(heapHandle, 0, pszFileText);
+            }
+        }
+        CloseHandle(hFile);
+    }
+    return bSuccess;
+}
+
+BOOL SaveTextFileFromEdit(HWND hEdit, LPCTSTR pszFileName)
+{
+    HANDLE hFile;
+    BOOL bSuccess = FALSE;
+
+    hFile = CreateFile(pszFileName, GENERIC_WRITE, 0, NULL,
+        CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+    if (hFile != INVALID_HANDLE_VALUE)
+    {
+        DWORD dwTextLength;
+
+        dwTextLength = GetWindowTextLength(hEdit);
+        if (dwTextLength > 0)
+        {
+            LPVOID pszText;
+            DWORD dwBufferSize = dwTextLength + 1;
+
+            HANDLE heapHandle = GetProcessHeap();
+            pszText = HeapAlloc(heapHandle, HEAP_GENERATE_EXCEPTIONS, dwBufferSize);
+            if (pszText != NULL)
+            {
+                if (GetWindowTextA(hEdit, (LPSTR)pszText, dwBufferSize))
+                {
+                    DWORD dwWritten;
+
+                    if (WriteFile(hFile, pszText, dwTextLength, &dwWritten, NULL))
+                        bSuccess = TRUE;
+                }
+                HeapFree(heapHandle, 0, pszText);
+            }
+        }
+        CloseHandle(hFile);
+    }
+    return bSuccess;
 }
--- tiny-notepad/tiny-notepad.h
+++ tiny-notepad/tiny-notepad.h
@@ -1,3 +1,23 @@
 #pragma once
 
 #include "resource.h"
+
+#define MAX_LOADSTRING 100
+#define IDC_MAIN_EDIT 200
+#define IDC_MAIN_TOOL 201
+#define IDC_MAIN_STATUS 202
+
+// Global Variables:
+HINSTANCE hInst;                                // current instance
+WCHAR szTitle[MAX_LOADSTRING];                  // The title bar text
+WCHAR szWindowClass[MAX_LOADSTRING];            // the main window class name
+
+                                                // Forward declarations of functions included in this code module:
+ATOM                MyRegisterClass(HINSTANCE hInstance);
+BOOL                InitInstance(HINSTANCE, int);
+LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
+void                DoFileOpen(HWND hWnd);
+void                DoFileSave(HWND hWnd);
+void                DoFileNew(HWND hWnd);
+BOOL                LoadTextFileToEdit(HWND hEdit, LPCTSTR pszFileName);
+BOOL                SaveTextFileFromEdit(HWND hEdit, LPCTSTR pszFileName);
